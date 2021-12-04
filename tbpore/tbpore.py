e5113f1ffe2f5ff0af0022ad3edc071d8cbd079f
Michael Hall <michael@mbh.sh>
2021-12-04 17:01:19 +1000

    add input argument and options

--- tbpore/tbpore.py
+++ tbpore/tbpore.py
@@ -1,5 +1,6 @@
 import sys
 from pathlib import Path
+from typing import Tuple
 
 import click
 from loguru import logger
@@ -40,9 +41,25 @@ log_fmt = (
     cls=Mutex,
     not_required_if=["verbose"],
 )
+@click.option(
+    "-r", "--recursive", help="Recursively search INPUT for fastq files", is_flag=True
+)
+@click.argument("input", type=click.Path(exists=True, path_type=Path), nargs=-1)
 @click.pass_context
-def main(ctx: click.Context, verbose: bool, quiet: bool, outdir: Path):
-    """Mycobacterium tuberculosis genomic analysis from Nanopore sequencing data"""
+def main(
+    ctx: click.Context,
+    verbose: bool,
+    quiet: bool,
+    outdir: Path,
+    input: Tuple[Path, ...],
+    recursive: bool,
+):
+    """Mycobacterium tuberculosis genomic analysis from Nanopore sequencing data
+
+    INPUT: Fastq file(s) and/or a directory containing fastq files. All files will
+    be joined into a single fastq file, so ensure thery're all part of the same
+    sample/isolate.
+    """
     log_lvl = "INFO"
     if verbose:
         log_lvl = "DEBUG"
@@ -51,6 +68,8 @@ def main(ctx: click.Context, verbose: bool, quiet: bool, outdir: Path):
     logger.remove()
     logger.add(sys.stderr, level=log_lvl, format=log_fmt)
     logger.info(f"Welcome to TBpore version {__version__}")
+
+    # todo: get full list of input files
 
 
 if __name__ == "__main__":
