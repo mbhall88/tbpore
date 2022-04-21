cf28d2d321a5bc8188edd0302929e3f5298d9b20
Leandro Ishi <leandro.ishi.lima@gmail.com>
2022-04-21 15:53:53 +0100

    Adding config_file location to constants

--- tbpore/constants.py
+++ tbpore/constants.py
@@ -4,4 +4,4 @@ repo_root = Path(__file__).parent.parent.resolve()
 H37RV_genome = repo_root / "data/H37RV_genome/h37rv.fa.gz"
 H37RV_mask = repo_root / "data/H37RV_genome/compass-mask.bed"
 cache_dir = repo_root / ".cache"
-config_file = repo_root / "config.yaml"
+config_file = repo_root / ".config.yaml"
