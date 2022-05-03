48786e7f6cf4e3b5764fdbcc453a0b24e8075ecb
Leandro Ishi <leandro.ishi.lima@gmail.com>
2022-05-03 10:13:34 +0100

    Adding rule compare_h2h_and_tbpore_consensus using psdm

---
+++ pipelines/snakemake/scripts/compare_H2H_and_tbpore_consensus.py
@@ -0,0 +1,30 @@
+from glob import glob
+from pathlib import Path
+import subprocess
+
+
+def get_samples(glob_pattern):
+    consensus_files = (Path(file).resolve() for file in glob(glob_pattern))
+    return {file.name.replace(".consensus.fa", ""): file for file in consensus_files}
+
+
+def compare(h2h_glob_pattern, tbpore_glob_pattern, output):
+    h2h_samples = get_samples(h2h_glob_pattern)
+    tbpore_samples = get_samples(tbpore_glob_pattern)
+    common_samples = set(h2h_samples.keys()).intersection(set(tbpore_samples.keys()))
+
+    for sample in common_samples:
+        h2h_consensus = h2h_samples[sample]
+        tbpore_consensus = tbpore_samples[sample]
+        subprocess.check_call(f"psdm -l -s -i -P -t 1 {h2h_consensus} {tbpore_consensus} >>{output}  2>/dev/null")
+
+
+def __main__():
+    with open(snakemake.output.consensus_comparison, "w"):
+        pass
+    compare(h2h_glob_pattern=snakemake.params.h2h_glob_pattern,
+            tbpore_glob_pattern=f"{snakemake.params.tbpore_output}/*/*.consensus.fa",
+            output=snakemake.output.consensus_comparison)
+
+
+__main__()
