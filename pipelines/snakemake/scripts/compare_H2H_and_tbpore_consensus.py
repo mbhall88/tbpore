4dbd024d8b14962a74f226ab352743a3df4340e1
Leandro Ishi <leandro.ishi.lima@gmail.com>
2022-05-03 11:05:02 +0100

    h2h_glob_pattern -> h2h_consensus_glob_pattern

--- pipelines/snakemake/scripts/compare_H2H_and_tbpore_consensus.py
+++ pipelines/snakemake/scripts/compare_H2H_and_tbpore_consensus.py
@@ -8,8 +8,8 @@ def get_samples(glob_pattern):
     return {file.name.replace(".consensus.fa", ""): file for file in consensus_files}
 
 
-def compare(h2h_glob_pattern, tbpore_glob_pattern, output):
-    h2h_samples = get_samples(h2h_glob_pattern)
+def compare(h2h_consensus_glob_pattern, tbpore_glob_pattern, output):
+    h2h_samples = get_samples(h2h_consensus_glob_pattern)
     tbpore_samples = get_samples(tbpore_glob_pattern)
     common_samples = set(h2h_samples.keys()).intersection(set(tbpore_samples.keys()))
 
@@ -22,7 +22,7 @@ def compare(h2h_glob_pattern, tbpore_glob_pattern, output):
 def __main__():
     with open(snakemake.output.consensus_comparison, "w"):
         pass
-    compare(h2h_glob_pattern=snakemake.params.h2h_glob_pattern,
+    compare(h2h_consensus_glob_pattern=snakemake.params.h2h_consensus_glob_pattern,
             tbpore_glob_pattern=f"{snakemake.params.tbpore_output}/*/*.consensus.fa",
             output=snakemake.output.consensus_comparison)
 
